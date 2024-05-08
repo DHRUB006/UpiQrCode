80988cbfc453a6fa0b7126eed510e27db121ef2f
-DHRUB_DXD <106004326+DHRUB006@users.noreply.github.com>
2024-05-08 23:44:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
   const qrCodeImage = document.getElementById("qrcode");
-  qrCodeImage.src = "https://iili.io/HyfD2xj.jpg";
+  qrCodeImage.src = "https://iili.io/Jr4oEG9.jpg";
 });
